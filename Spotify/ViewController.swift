9f4053bb5167f40a9fd7858e73d6231db13fdadb
Ramazan Abdullayev <ramazan.aev@gmail.com>
2021-04-14 08:55:16 +0400

    first commit :-)

---
+++ Spotify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by Ramazan Abdullayev on 14.04.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

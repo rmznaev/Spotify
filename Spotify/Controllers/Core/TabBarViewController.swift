7dc4ef8ca3086d21d3a58f21a4fb6ee3a80c137e
Ramazan Abdullayev <ramazan.aev@gmail.com>
2021-04-15 14:00:50 +0400

    added vc files

---
+++ Spotify/Controllers/Core/TabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TabBarViewController.swift
+//  Spotify
+//
+//  Created by Ramazan Abdullayev on 15.04.21.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}

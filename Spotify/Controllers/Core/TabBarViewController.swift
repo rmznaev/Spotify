dc356c181ec3f3bc47b7f90aa73cb8ebc0d87f1b
Ramazan Abdullayev <ramazan.aev@gmail.com>
2021-04-16 09:03:24 +0400

    added tab bar and its view controllers

--- Spotify/Controllers/Core/TabBarViewController.swift
+++ Spotify/Controllers/Core/TabBarViewController.swift
@@ -12,6 +12,30 @@ class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let vcOne = HomeViewController()
+        let vcTwo = SearchViewController()
+        let vcThree = LibraryViewController()
+        
+        vcOne.title = "Browse"
+        vcTwo.title = "Search"
+        vcThree.title = "Library"
+        
+        vcOne.navigationItem.largeTitleDisplayMode = .always
+        vcTwo.navigationItem.largeTitleDisplayMode = .always
+        vcThree.navigationItem.largeTitleDisplayMode = .always
+        
+        let navOne = UINavigationController(rootViewController: vcOne)
+        let navTwo = UINavigationController(rootViewController: vcTwo)
+        let navThree = UINavigationController(rootViewController: vcThree)
+        
+        navOne.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 1)
+        navTwo.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 1)
+        navThree.tabBarItem = UITabBarItem(title: "Library", image: UIImage(systemName: "music.note.list"), tag: 1)
+        
+        navOne.navigationBar.prefersLargeTitles = true
+        navTwo.navigationBar.prefersLargeTitles = true
+        navThree.navigationBar.prefersLargeTitles = true
+        
+        setViewControllers([navOne, navTwo, navThree], animated: false)
     }
 }

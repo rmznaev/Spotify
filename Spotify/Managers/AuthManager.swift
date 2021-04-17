5717f69e8390bdbe364fcd8463ae3e993cfacb58
Ramazan Abdullayev <ramazan.aev@gmail.com>
2021-04-17 15:03:40 +0400

    added some part of the signing up code

--- Spotify/Managers/AuthManager.swift
+++ Spotify/Managers/AuthManager.swift
@@ -17,6 +17,15 @@ final class AuthManager {
     
     private init() {}
     
+    public var signInURL: URL? {
+        let baseURL = "https://accounts.spotify.com/authorize"
+        let scopes = "user-read-private"
+        let redirectURI = "https://www.iosacademy.io"
+        let string = "\(baseURL)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        
+        return URL(string: string)
+    }
+    
     var isSignedIn: Bool {
         return false
     }
@@ -35,5 +44,17 @@ final class AuthManager {
     
     private var shouldRefreshToken: Bool {
         return false
+    }
+    
+    public func exchangeCodeForToken(code: String, completion: @escaping ((Bool) -> Void)) {
+        // Get token
+    }
+    
+    public func refreshAccessToken() {
+        
+    }
+    
+    private func cacheToken() {
+        
     }
 }
